a2bfac5abbaa12521aeba7afb9c9202edce4caf4
Yusuf Muradov <surgeon33NN@yandex.ru>
2024-11-16 18:11:30 +0300

    fix comments

--- src/history/InMemoryHistoryManager.java
+++ src/history/InMemoryHistoryManager.java
@@ -3,16 +3,18 @@ package history;
 import models.Task;
 import utils.CopyUtils;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final List<Task> taskHistory = new ArrayList<>();
+    private final List<Task> taskHistory = new LinkedList<>();
+
+    private static final int MAX_SIZE = 10;
 
     @Override
     public void add(Task task) {
-        if (taskHistory.size() == 10) {
+        if (taskHistory.size() == MAX_SIZE) {
             taskHistory.removeFirst();
         }
         Task copy = CopyUtils.copyForHistory(task);
--- src/utils/Managers.java
+++ src/utils/Managers.java
@@ -7,6 +7,10 @@ import manager.TaskManager;
 
 public class Managers {
 
+    private Managers() {
+        throw new IllegalStateException("This is an utility Class and cannot be instantiated");
+    }
+
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
