18b96829129293b6d7f174c71ad18a4eda9ca81c
Yusuf Muradov <surgeon33NN@yandex.ru>
2025-01-18 15:02:36 +0300

    sprint_8-solution-time-and-duration: исправить чекстайл

--- src/test/java/manager/FileBackedTaskManagerTest.java
+++ src/test/java/manager/FileBackedTaskManagerTest.java
@@ -2,7 +2,7 @@ package manager;
 
 import java.nio.file.Path;
 
-public class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager>{
+public class FileBackedTaskManagerTest extends TaskManagerTest <FileBackedTaskManager> {
 
     public FileBackedTaskManagerTest() {
         super(new FileBackedTaskManager(Path.of("src/test/resources/tasks.csv")));
