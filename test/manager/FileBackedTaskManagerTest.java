047628423ec4f7a738f1cdd1fb5e42ecb16e1bcf
Yusuf Muradov <surgeon33NN@yandex.ru>
2025-01-18 15:08:18 +0300

    sprint_8-solution-time-and-duration: вернуть пакетное расположение

--- test/manager/FileBackedTaskManagerTest.java
+++ test/manager/FileBackedTaskManagerTest.java
@@ -5,6 +5,6 @@ import java.nio.file.Path;
 public class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskManager> {
 
     public FileBackedTaskManagerTest() {
-        super(new FileBackedTaskManager(Path.of("src/test/resources/tasks.csv")));
+        super(new FileBackedTaskManager(Path.of("src/main/resources/tasks.csv")));
     }
 }
